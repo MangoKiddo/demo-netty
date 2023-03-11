7e2489fd9340cac56cdfe30c5dfd92402069988b
wukai <mangokiddo@163.com>
2023-03-11 22:32:33 +0800

    feat初始化netty并接收数据

---
+++ netty-01/src/main/java/com/mangokiddo/netty/server/NettyServer.java
@@ -0,0 +1,7 @@
+package com.mangokiddo.netty.server;/**
+ * @description: 
+ * @author: mango
+ * @time: 2023/3/10 18:30
+ */
+public class NettyServer {
+}
--- netty-01/src/main/java/netty01demo.java
+++
@@ -1,7 +0,0 @@
-/**
- * @description:
- * @author: mango
- * @time: 2023/3/10 13:47
- */
-public class netty01demo {
-}
